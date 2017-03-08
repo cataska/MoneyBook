0f6d2bdc1b54b504bd6c20f8cf85898b38708a24
Wen-Chun Lin <lovecankill@gmail.com>
2017-03-08 12:58:34 +0800

    Add Item class

---
+++ WebApplication1/WebApplication1/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Item
+    {
+        public int Type { get; set; }
+        public float Value { get; set; }
+        public DateTime Created { get; set; }
+        public string Note { get; set; }
+    }
+}
