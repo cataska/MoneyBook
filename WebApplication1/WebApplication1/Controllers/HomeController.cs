fffb881006b201268072dba2c64de6797dbec87f
Wen-Chun Lin <lovecankill@gmail.com>
2017-03-13 21:13:55 +0800

    Move business logic in HomeController to AccountBookController

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,26 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebApplication1.Models;
-using WebApplication1.Models.ViewModels;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private SkillTreeHomeworkEntities db = new SkillTreeHomeworkEntities();
-
         public ActionResult Index()
         {
-            var models = db.AccountBook.Select(c => new AccountViewModel()
-            {
-                Value = c.Amounttt,
-                Created = c.Dateee,
-                Note = c.Remarkkk,
-                Type = c.Categoryyy == 0 ? "支出" : "收入"
-            }).ToList();
-
-            return View(models);
+            return View();
         }
 
         public ActionResult About()
