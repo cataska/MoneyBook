9d3e765e05af11587e0587703ccf14cd99662db5
Wen-Chun Lin <lovecankill@gmail.com>
2017-03-21 21:13:41 +0800

    Add annotations for display and validation

--- WebApplication1/WebApplication1/Models/ViewModels/AccountViewModel.cs
+++ WebApplication1/WebApplication1/Models/ViewModels/AccountViewModel.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models.ViewModels
 {
+    public enum CategoryEnum
+    {
+        支出,
+        收入
+    }
+
     public class AccountViewModel
     {
-        public string Type { get; set; }
+        [Display(Name = "類別")]
+        public CategoryEnum Category { get; set; }
+
+        [Display(Name = "金額")]
+        [Required]
+        [Range(1, int.MaxValue)]
         public float Value { get; set; }
+
+        [Display(Name = "日期")]
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime Created { get; set; }
+
+        [Display(Name = "備註")]
+        [StringLength(100)]
         public string Note { get; set; }
     }
 }
