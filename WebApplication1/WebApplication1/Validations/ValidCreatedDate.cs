5196c42090a8854017e084e2039e7fa42f8ebbb8
Wen-Chun Lin <lovecankill@gmail.com>
2017-03-22 14:21:52 +0800

    Add validation for created date

---
+++ WebApplication1/WebApplication1/Validations/ValidCreatedDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Validations
+{
+    public sealed class ValidCreatedDate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult(ErrorMessage ?? DefaultErrorMessage);
+            }
+
+            var created = Convert.ToDateTime(value);
+            return created > DateTime.Today ? new ValidationResult(ErrorMessage ?? DefaultErrorMessage) : ValidationResult.Success;
+        }
+
+        private const string DefaultErrorMessage = "Must equal or greater than tody";
+    }
+}
